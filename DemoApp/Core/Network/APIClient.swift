71aa830b0908b74bc5c3ab74fba6fc8b76173e2a
hamza <hamzon.jb@gmail.com>
2024-05-05 10:21:01 +0200

    wip Network layer

---
+++ DemoApp/Core/Network/APIClient.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Combine
+
+protocol APIClient {
+    associatedtype EndpointType: APIEndpoint
+    func request<T: Decodable>(_ endpoint: EndpointType) -> AnyPublisher<T, Error>
+}
