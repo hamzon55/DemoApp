ea6ab95a4b284a5432e2fc575cee703278cf467d
hamza <hamzon.jb@gmail.com>
2024-05-09 18:05:09 +0200

    Removed extra spaces

--- DemoApp/Modules/FirstScreen/FirstViewController.swift
+++ DemoApp/Modules/FirstScreen/FirstViewController.swift
@@ -8,44 +8,40 @@
 import UIKit
 
 class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-
+    
     var coordinator: AppCoordinator?
-
+    
     private lazy var tableView: UITableView = {
-          let tableView = UITableView(frame: view.bounds, style: .plain)
-          tableView.delegate = self
-          tableView.dataSource = self
-          return tableView
-      }()
-
-
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.red
         view.addSubview(tableView)
-
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           // Return the number of rows in your table
-           return 10
-       }
-
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           // Dequeue or create a new cell
-           let cell = UITableViewCell(style: .default, reuseIdentifier: "cellIdentifier")
-           
-           // Configure the cell
-           cell.textLabel?.text = "Row \(indexPath.row)"
-           
-           return cell
-       }
-
-       // MARK: - UITableViewDelegate methods
-       
-       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           // Handle row selection
-           print("Selected row \(indexPath.row)")
-       }
+        // Return the number of rows in your table
+        return 10
+    }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue or create a new cell
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellIdentifier")
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate methods
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected row \(indexPath.row)")
+    }
 }
