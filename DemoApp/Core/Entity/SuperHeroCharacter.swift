02c7b597ff17e4e448b60153be5e7fcf8e6fea74
hamza <hamzon.jb@gmail.com>
2024-05-10 16:41:26 +0200

    Added Simple Hero Model and Simple ViewState

---
+++ DemoApp/Core/Entity/SuperHeroCharacter.swift
@@ -0,0 +1,14 @@
+//
+//  SuperHeroCharacter.swift
+//  DemoApp
+//
+//  Created by HamZa Jerbi on 10/5/24.
+//
+
+import Foundation
+
+struct MarvelCharacter: Codable,Hashable {
+    
+    let name: String?
+    let description: String?
+}
---
+++ DemoApp/Modules/FirstScreen/FirstViewState.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+enum FirstViewState {
+
+    case idle
+    case loading
+    case failed(Error)
+    
+}
