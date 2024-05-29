e43dc5d44bd4cbcfb2cd4e150a939807eec8998c
hamza <hamzon.jb@gmail.com>
2024-05-29 19:36:21 +0200

    Wip HeroDetail

---
+++ DemoApp/Modules/HeroDetails/HeroDetailViewModel.swift
@@ -0,0 +1,13 @@
+import Combine
+import UIKit
+
+typealias HeroDetailViewModelOuput = AnyPublisher<HeroDetailViewState, Never>
+
+class HeroDetailViewModel: HeroeDetailViewModelType {
+    
+    func transform(input: HeroDetailViewModelInput) -> HeroDetailViewModelOuput {
+        
+    }
+    
+    
+}
---
+++ DemoApp/Modules/HeroDetails/HeroDetailViewState.swift
@@ -0,0 +1,26 @@
+import Combine
+protocol HeroeDetailViewModelType: AnyObject {
+    func transform(input: HeroDetailViewModelInput) -> HeroDetailViewModelOuput
+}
+
+
+public enum HeroDetailViewState {
+    case idle
+    case success([Character])
+    case error(String)
+}
+
+extension HeroDetailViewState: Equatable {
+    public static func == (lhs: HeroDetailViewState, rhs: HeroDetailViewState) -> Bool {
+        switch (lhs, rhs) {
+        case (.success(let lhsSeries), .success(let rhsSeries)): return lhsSeries == rhsSeries
+        case (.error, .error): return true
+        default: return false
+        }
+    }
+}
+
+struct HeroDetailViewModelInput {
+    let appear: AnyPublisher<Void, Never>
+}
+
