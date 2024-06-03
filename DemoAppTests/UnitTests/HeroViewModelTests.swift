722e2a75500a857a08c9c7d76329e7d0a8a46db8
hamza <hamzon.jb@gmail.com>
2024-06-03 17:49:29 +0200

    Refactor Tests

--- DemoAppTests/UnitTests/HeroViewModelTests.swift
+++ DemoAppTests/UnitTests/HeroViewModelTests.swift
@@ -25,23 +25,29 @@ final class HeroViewModelTests: XCTestCase {
            super.tearDown()
        }
     
+    private func createInput(appear: AnyPublisher<Void, Never> = Just(()).eraseToAnyPublisher(),
+                                search: AnyPublisher<String, Never> = PassthroughSubject<String, Never>().eraseToAnyPublisher(),
+                                loadMore: AnyPublisher<Void, Never> = PassthroughSubject<Void, Never>().eraseToAnyPublisher(),
+                                selection: AnyPublisher<Int, Never> = PassthroughSubject<Int, Never>().eraseToAnyPublisher()) -> HeroViewModelInput {
+        return HeroViewModelInput(appear: appear, selection: selection, search: search, loadMore: loadMore)
+       }
+    
+    private func createResponse(offset: Int, limit: Int, total: Int, count: Int, characters: [Character]) -> MarvelResponse {
+           return MarvelResponse(data: HeroClass(offset: offset, limit: limit, total: total, count: count, results: characters))
+       }
+    
     func testFetchHeroesOnAppear() {
            let expectation = XCTestExpectation(description: "Fetch heroes on appear")
            
-           let response = MarvelResponse(data: HeroClass(offset: 0, limit: 20, total: 100, count: 20, results: [
-            Character(id: 0, name: "Hero 1", description: "Description 1", thumbnail: Thumbnail(path: "path1", thumbnailExtension: "jpg")),
-            Character(id: 1, name: "Hero 2", description: "Description 2", thumbnail: Thumbnail(path: "path2", thumbnailExtension: "jpg"))
-           ]))
+           let response = createResponse(offset: 0, limit: 20, total: 100, count: 20, characters: [
+               Character(id: 0, name: "Hero 1", description: "Description 1", thumbnail: Thumbnail(path: "path1", thumbnailExtension: "jpg")),
+               Character(id: 1, name: "Hero 2", description: "Description 2", thumbnail: Thumbnail(path: "path2", thumbnailExtension: "jpg"))
+           ])
            mockHeroUseCase.getHeroesResult = Just(response)
                .setFailureType(to: APIError.self)
                .eraseToAnyPublisher()
            
-           let input = HeroViewModelInput(
-            appear: Just(()).eraseToAnyPublisher(),
-            selection:  PassthroughSubject<Int, Never>().eraseToAnyPublisher(),
-            search: PassthroughSubject<String, Never>().eraseToAnyPublisher(),
-            loadMore: PassthroughSubject<Void, Never>().eraseToAnyPublisher()
-           )
+           let input = createInput()
            
            let expectedItems = HeroScreenViewModel(heroes: [
                HeroItemCellViewModel(name: "Hero 1", description: "Description 1", characterImageURL: URL(string: "path1.jpg")),
@@ -59,57 +65,52 @@ final class HeroViewModelTests: XCTestCase {
            
            wait(for: [expectation], timeout: 1.0)
        }
-    
-    func testLoadMoreHeroes() {
-         let expectation = XCTestExpectation(description: "Load more heroes")
-         
-         let initialResponse = MarvelResponse(data: HeroClass(offset: 0, limit: 20, total: 100, count: 20, results: [
-            Character(id: 0, name: "Hero 1", description: "Description 1", thumbnail: Thumbnail(path: "path1", thumbnailExtension: "jpg")),
-            Character(id: 0, name: "Hero 2", description: "Description 2", thumbnail: Thumbnail(path: "path2", thumbnailExtension: "jpg")),
-         ]))
-         let loadMoreResponse = MarvelResponse(data: HeroClass(offset: 20, limit: 20, total: 100, count: 20, results: [
-            Character(id: 0, name: "Hero 3", description: "Description 3", thumbnail: Thumbnail(path: "path3", thumbnailExtension: "jpg")),
-            Character(id: 0, name: "Hero 4", description: "Description 4", thumbnail: Thumbnail(path: "path4", thumbnailExtension: "jpg")),
-         ]))
-         
-         mockHeroUseCase.getHeroesResult = Just(initialResponse)
-             .setFailureType(to: APIError.self)
-             .eraseToAnyPublisher()
-         
-         let loadMoreSubject = PassthroughSubject<Void, Never>()
-         
-         let input = HeroViewModelInput(
-            appear: Just(()).eraseToAnyPublisher(),
-            selection: PassthroughSubject<Int, Never>().eraseToAnyPublisher(),
-             search: PassthroughSubject<String, Never>().eraseToAnyPublisher(),
-             loadMore: loadMoreSubject.eraseToAnyPublisher()
-         )
-         
-         viewModel.transform(input: input)
-             .sink(receiveCompletion: { _ in }, receiveValue: { state in
-                 if case .success(let items) = state, items.heroes.count == 4 {
-                     XCTAssertEqual(items.heroes[0].name, "Hero 1")
-                     XCTAssertEqual(items.heroes[1].name, "Hero 2")
-                     XCTAssertEqual(items.heroes[2].name, "Hero 3")
-                     XCTAssertEqual(items.heroes[3].name, "Hero 4")
-                     expectation.fulfill()
-                 }
-             })
-             .store(in: &cancellables)
-         
-         // Send the initial appear event
-         viewModel.transform(input: input)
-             .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
-             .store(in: &cancellables)
-         
-         // Update the result to the load more response
-         mockHeroUseCase.getHeroesResult = Just(loadMoreResponse)
-             .setFailureType(to: APIError.self)
-             .eraseToAnyPublisher()
-         
-         // Send the load more event
-         loadMoreSubject.send(())
-         
-         wait(for: [expectation], timeout: 2.0)
-     }
- }
+       
+       func testLoadMoreHeroes() {
+           let expectation = XCTestExpectation(description: "Load more heroes")
+           
+           let initialResponse = createResponse(offset: 0, limit: 20, total: 100, count: 20, characters: [
+               Character(id: 0, name: "Hero 1", description: "Description 1", thumbnail: Thumbnail(path: "path1", thumbnailExtension: "jpg")),
+               Character(id: 1, name: "Hero 2", description: "Description 2", thumbnail: Thumbnail(path: "path2", thumbnailExtension: "jpg"))
+           ])
+           let loadMoreResponse = createResponse(offset: 20, limit: 20, total: 100, count: 20, characters: [
+               Character(id: 2, name: "Hero 3", description: "Description 3", thumbnail: Thumbnail(path: "path3", thumbnailExtension: "jpg")),
+               Character(id: 3, name: "Hero 4", description: "Description 4", thumbnail: Thumbnail(path: "path4", thumbnailExtension: "jpg"))
+           ])
+           
+           mockHeroUseCase.getHeroesResult = Just(initialResponse)
+               .setFailureType(to: APIError.self)
+               .eraseToAnyPublisher()
+           
+           let loadMoreSubject = PassthroughSubject<Void, Never>()
+           
+           let input = createInput(loadMore: loadMoreSubject.eraseToAnyPublisher())
+           
+           viewModel.transform(input: input)
+               .sink(receiveCompletion: { _ in }, receiveValue: { state in
+                   if case .success(let items) = state, items.heroes.count == 4 {
+                       XCTAssertEqual(items.heroes[0].name, "Hero 1")
+                       XCTAssertEqual(items.heroes[1].name, "Hero 2")
+                       XCTAssertEqual(items.heroes[2].name, "Hero 3")
+                       XCTAssertEqual(items.heroes[3].name, "Hero 4")
+                       expectation.fulfill()
+                   }
+               })
+               .store(in: &cancellables)
+           
+           // Send the initial appear event
+           viewModel.transform(input: input)
+               .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+               .store(in: &cancellables)
+           
+           // Update the result to the load more response
+           mockHeroUseCase.getHeroesResult = Just(loadMoreResponse)
+               .setFailureType(to: APIError.self)
+               .eraseToAnyPublisher()
+           
+           // Send the load more event
+           loadMoreSubject.send(())
+           
+           wait(for: [expectation], timeout: 2.0)
+       }
+   }
